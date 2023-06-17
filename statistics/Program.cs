29e3cb73cbd116aff6988b7359dc25fd2d638a34
azizlibayim <bayimsha@code.edu.az>
2023-06-17 07:18:16 +0400

    first commit

---
+++ statistics/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp22
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student count: ");
+            int s = int.Parse(Console.ReadLine());
+
+            string[] Students = new string[s];
+            int[] Grades = new int[s];
+
+            for (int i = 0; i < Students.Length; i++)
+            {
+                Console.WriteLine("Enter the student's name: ");
+                Students[i] = Console.ReadLine();
+            }
+
+            for (int j = 0; j < Grades.Length; j++)
+            {
+                Console.WriteLine("Enter student's grade: ");
+                Grades[j] = int.Parse(Console.ReadLine());
+            }
+
+            StatisticsHelper statistics = new StatisticsHelper(Grades);
+
+            Console.WriteLine("Student List:");
+            for (int k = 0; k < s; k++)
+            {
+                Console.WriteLine($"Name: {Students[k]}, Grade: {Grades[k]}");
+            }
+
+            Console.WriteLine($"Average Grade: {statistics.GetAverageGrade()}");
+            Console.WriteLine($"Lowest Grade: {statistics.GetLowestGrade()}");
+            Console.WriteLine($"Highest Grade: {statistics.GetHighestGrade()}");
+        }
+
+    
+}
---
+++ statistics/StatisticsHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class StatisticsHelper
+    {
+        
+            private int[] grades;
+
+            public StatisticsHelper(int[] grades)
+            {
+                this.grades = grades;
+            }
+
+            public float GetAverageGrade()
+            {
+                int sum = 0;
+                foreach (int grade in grades)
+                {
+                    sum += grade;
+                }
+
+                return (float)sum / grades.Length;
+            }
+
+            public int GetLowestGrade()
+            {
+                int minGrade = int.MaxValue;
+                foreach (int grade in grades)
+                {
+                    if (grade < minGrade)
+                    {
+                        minGrade = grade;
+                    }
+                }
+
+                return minGrade;
+            }
+
+            public int GetHighestGrade()
+            {
+                int maxGrade = int.MinValue;
+                foreach (int grade in grades)
+                {
+                    if (grade > maxGrade)
+                    {
+                        maxGrade = grade;
+                    }
+                }
+
+                return maxGrade;
+            }
+        }
+
+    }
